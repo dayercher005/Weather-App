dc0cf6e3c90a107766f79dd86620ccd8c969b06e
Dayer <dayerxr.cher@gmail.com>
2025-07-11 15:19:09 +0800

    Rendering UI for Weather App

--- src/API.js
+++ src/API.js
@@ -4,6 +4,11 @@ async function LocationFetcher(location) {
     const WeatherDetailsJSON = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=FN5JYKF3GDMMFET2J2CG6W7WE`,{
         mode: 'cors'
     });
-    const WeatherDetails = WeatherDetailsJSON.json(); 
-    console.log(WeatherDetails)
+    const WeatherDetails = await WeatherDetailsJSON.json(); 
+    const weatherConditions = await WeatherDetails.currentConditions.conditions;
+    const temperature = WeatherDetails.currentConditions.temp;
+    const weatherDescription = WeatherDetails.currentConditions.description;
+    console.log(WeatherDetails);
+    return {weatherConditions, temperature, weatherDescription}
 }
+
